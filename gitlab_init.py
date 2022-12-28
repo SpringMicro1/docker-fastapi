b2a81bbd8e6f47c9bfabef7dd94e18c5b41dd35b
David Buckley <runalldayandnight@gmail.com>
2022-12-28 16:27:09 -0700

    fix CI name

--- gitlab_init.py
+++
@@ -1,4 +0,0 @@
-URL = input("Enter the URL for the GitLab instance (default: http://localhost:8980)")
-URL = URL if URL else "http://localhost:8980"
-PROJECT_NAME = input("Enter the project name (default: docker-fastapi)")
-PROJECT_NAME = PROJECT_NAME if PROJECT_NAME else "docker-fastapi"
